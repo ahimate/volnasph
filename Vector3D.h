d0596504f98e3ab8b5bcb98a1600f7b23aa40816
ilyakrukov <ilyakrukov@a33fa313-bfd6-eac0-14a0-bde52577ac6f>
2010-08-26 16:48:46 +0000

    Vector3D class modification

--- Vector3D.h
+++ Vector3D.h
@@ -2,36 +2,50 @@
  * Vector3D.h
  *
  *  Created on: 03.06.2010
- *      Author: ilya
+ *  Updated on: 26.08.2010
+ *      Author: Ilya Kryukov
  */
 
 #ifndef VECTOR3D_H_
 #define VECTOR3D_H_
 #include <cmath>
 
-//Объект представляющий вектор или точку в пространстве
+// Vector or point in 3d space
+// aligned by 16 byte w - padding element
 class Vector3D {
-private:
+public:
 	double x;
 	double y;
 	double z;
-public:
+	double w; // padding
+
+	//constructors
 	Vector3D();
 	Vector3D(double x, double y, double z);
+
+	//getters-setters
 	double getX() const;
 	double getY() const;
 	double getZ() const;
 	void setX(double x);
 	void setY(double y);
 	void setZ(double z);
+
+	double abs(void) const;
+
 	friend Vector3D operator+(Vector3D& a, Vector3D& b);
 	friend Vector3D operator-(Vector3D& a, Vector3D& b);
 	friend Vector3D operator*(Vector3D& a, double k);
 	friend Vector3D operator*(double k, Vector3D& a);
-	friend Vector3D scalarMul(Vector3D& a, Vector3D& b);
-	friend Vector3D vectorMul(Vector3D& a, Vector3D& b);
-	double abs(void) const;
+
+	friend Vector3D operator+=(Vector3D& a, Vector3D& b);
+	friend Vector3D operator-=(Vector3D& a, Vector3D& b);
+	//destructor
 	virtual ~Vector3D();
 };
+
+//Math functions for vector
+Vector3D scalarMul(Vector3D& a, Vector3D& b);
+Vector3D vectorMul(Vector3D& a, Vector3D& b);
 
 #endif /* VECTOR3D_H_ */
